af703b2dcb0e6581c8ba52086282dccc66e728a9
Matthias Geier <Matthias.Geier@gmail.com>
2020-08-21 17:23:21 +0200

    TST: Test case for static arrays of strings

---
+++ tests/strings.rs
@@ -0,0 +1,11 @@
+use std::ffi::CStr;
+
+use libflac_sys as ffi;
+
+#[test]
+fn strings() {
+    let idx = ffi::FLAC__STREAM_METADATA_PICTURE_TYPE_FISH as usize;
+    let ptr = unsafe { *ffi::FLAC__StreamMetadata_Picture_TypeString.get_unchecked(idx) };
+    let c_string = unsafe { CStr::from_ptr(ptr) };
+    assert_eq!(c_string.to_str().unwrap(), "A bright coloured fish");
+}
