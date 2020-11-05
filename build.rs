739bbe3fc7bb9925485ec4985d1fadee33086aba
наб <nabijaczleweli@nabijaczleweli.xyz>
2020-11-05 17:20:18 +0100

    Remove unused CMake defines. Only build libFLAC instead of the additional executables
    This makes the crate build and pass tests on my msys2 platform
    
    Ref: https://github.com/nabijaczleweli/flac-bound/pull/2

--- build.rs
+++ build.rs
@@ -2,14 +2,9 @@ fn main() {
     let mut flac_config = cmake::Config::new("flac");
     flac_config
         .define("BUILD_CXXLIBS", "OFF")
-        .define("BUILD_PROGRAMS", "OFF")
         .define("BUILD_EXAMPLES", "OFF")
-        .define("BUILD_DOCS", "OFF")
         .define("BUILD_DOXYGEN", "OFF")
-        .define("BUILD_TESTING", "OFF")
-        .define("INSTALL_MANPAGES", "OFF")
-        .define("INSTALL_PKGCONFIG_MODULES", "OFF")
-        .define("INSTALL_CMAKE_CONFIG_MODULE", "OFF");
+        .define("BUILD_TESTING", "OFF");
 
     if cfg!(feature = "ogg") {
         let ogg_path = cmake::Config::new("ogg")
@@ -20,13 +15,96 @@ fn main() {
             .build();
         println!("cargo:rustc-link-search=native={}/lib", ogg_path.display());
         println!("cargo:rustc-link-lib=static=ogg");
+
         flac_config.define("CMAKE_PREFIX_PATH", ogg_path);
         flac_config.define("WITH_OGG", "ON");
     } else {
         flac_config.define("WITH_OGG", "OFF");
     };
 
-    let flac_path = flac_config.build();
-    println!("cargo:rustc-link-search=native={}/lib", flac_path.display());
+    let flac_path = flac_config.build_target("FLAC").build();
+    if std::env::var("TARGET")
+        .expect("TARGET is defined by rustc")
+        .contains("msvc")
+    {
+        // cmake automatically picks the build profile, and MSVC binary location depends thereon;
+        // this code is borrowed from https://docs.rs/cmake/0.1.44/src/cmake/lib.rs.html#498,
+        // but see https://github.com/alexcrichton/cmake-rs/pull/105 for a replacement
+        let profile = {
+            // Automatically set the `CMAKE_BUILD_TYPE` if the user did not
+            // specify one.
+
+            // Determine Rust's profile, optimization level, and debug info:
+            #[derive(PartialEq)]
+            enum RustProfile {
+                Debug,
+                Release,
+            }
+            #[derive(PartialEq, Debug)]
+            enum OptLevel {
+                Debug,
+                Release,
+                Size,
+            }
+
+            let rust_profile =
+                match &std::env::var("PROFILE").expect("PROFILE is defined by rustc")[..] {
+                    "debug" => RustProfile::Debug,
+                    "release" | "bench" => RustProfile::Release,
+                    unknown => {
+                        eprintln!(
+                            "Warning: unknown Rust profile={}; defaulting to a release build.",
+                            unknown
+                        );
+                        RustProfile::Release
+                    }
+                };
+
+            let opt_level =
+                match &std::env::var("OPT_LEVEL").expect("OPT_LEVEL is defined by rustc")[..] {
+                    "0" => OptLevel::Debug,
+                    "1" | "2" | "3" => OptLevel::Release,
+                    "s" | "z" => OptLevel::Size,
+                    unknown => {
+                        let default_opt_level = match rust_profile {
+                            RustProfile::Debug => OptLevel::Debug,
+                            RustProfile::Release => OptLevel::Release,
+                        };
+                        eprintln!(
+                            "Warning: unknown opt-level={}; defaulting to a {:?} build.",
+                            unknown, default_opt_level
+                        );
+                        default_opt_level
+                    }
+                };
+
+            let debug_info = match &std::env::var("DEBUG").expect("DEBUG is defined by rustc")[..] {
+                "false" => false,
+                "true" => true,
+                unknown => {
+                    eprintln!("Warning: unknown debug={}; defaulting to `true`.", unknown);
+                    true
+                }
+            };
+
+            match (opt_level, debug_info) {
+                (OptLevel::Debug, _) => "Debug",
+                (OptLevel::Release, false) => "Release",
+                (OptLevel::Release, true) => "RelWithDebInfo",
+                (OptLevel::Size, _) => "MinSizeRel",
+            }
+        };
+
+        println!(
+            "cargo:rustc-link-search=native={}/build/src/libFLAC/{}",
+            flac_path.display(),
+            profile
+        );
+    } else {
+        println!(
+            "cargo:rustc-link-search=native={}/build/src/libFLAC",
+            flac_path.display()
+        );
+    }
     println!("cargo:rustc-link-lib=static=FLAC");
 }
