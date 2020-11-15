981c9639a29304e28736e30fdb095fe8e78e8b05
Matthias Geier <Matthias.Geier@gmail.com>
2020-11-15 20:51:56 +0100

    TST: Print OGG support if "build-flac" is not enabled

--- tests/ogg_support.rs
+++ tests/ogg_support.rs
@@ -1,10 +1,9 @@
 #[test]
-#[cfg(feature = "build-flac")]
 fn ogg_support() {
-    unsafe {
-        assert_eq!(
-            libflac_sys::FLAC_API_SUPPORTS_OGG_FLAC,
-            cfg!(feature = "build-ogg").into()
-        );
+    let supports_ogg = unsafe { libflac_sys::FLAC_API_SUPPORTS_OGG_FLAC == 1 };
+    if cfg!(feature = "build-flac") {
+        assert_eq!(supports_ogg, cfg!(feature = "build-ogg"));
+    } else {
+        println!("external libFLAC supports OGG: {}", supports_ogg);
     }
 }
