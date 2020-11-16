f487737566e45cb68ab9e439f26cc5b30e2e6e40
наб <nabijaczleweli@nabijaczleweli.xyz>
2020-11-16 17:25:09 +0100

    Replace stolen implementation with cmake::Config::get_profile()
    Ref: #5

--- build.rs
+++ build.rs
@@ -33,78 +33,10 @@ fn main() {
         .expect("TARGET is defined by rustc")
         .contains("msvc")
     {
-        // cmake automatically picks the build profile, and MSVC binary location depends thereon;
-        // this code is borrowed from https://docs.rs/cmake/0.1.44/src/cmake/lib.rs.html#498,
-        // but see https://github.com/alexcrichton/cmake-rs/pull/105 for a replacement
-        let profile = {
-            // Automatically set the `CMAKE_BUILD_TYPE` if the user did not
-            // specify one.
-
-            // Determine Rust's profile, optimization level, and debug info:
-            #[derive(PartialEq)]
-            enum RustProfile {
-                Debug,
-                Release,
-            }
-            #[derive(PartialEq, Debug)]
-            enum OptLevel {
-                Debug,
-                Release,
-                Size,
-            }
-
-            let rust_profile =
-                match &std::env::var("PROFILE").expect("PROFILE is defined by rustc")[..] {
-                    "debug" => RustProfile::Debug,
-                    "release" | "bench" => RustProfile::Release,
-                    unknown => {
-                        eprintln!(
-                            "Warning: unknown Rust profile={}; defaulting to a release build.",
-                            unknown
-                        );
-                        RustProfile::Release
-                    }
-                };
-
-            let opt_level =
-                match &std::env::var("OPT_LEVEL").expect("OPT_LEVEL is defined by rustc")[..] {
-                    "0" => OptLevel::Debug,
-                    "1" | "2" | "3" => OptLevel::Release,
-                    "s" | "z" => OptLevel::Size,
-                    unknown => {
-                        let default_opt_level = match rust_profile {
-                            RustProfile::Debug => OptLevel::Debug,
-                            RustProfile::Release => OptLevel::Release,
-                        };
-                        eprintln!(
-                            "Warning: unknown opt-level={}; defaulting to a {:?} build.",
-                            unknown, default_opt_level
-                        );
-                        default_opt_level
-                    }
-                };
-
-            let debug_info = match &std::env::var("DEBUG").expect("DEBUG is defined by rustc")[..] {
-                "false" => false,
-                "true" => true,
-                unknown => {
-                    eprintln!("Warning: unknown debug={}; defaulting to `true`.", unknown);
-                    true
-                }
-            };
-
-            match (opt_level, debug_info) {
-                (OptLevel::Debug, _) => "Debug",
-                (OptLevel::Release, false) => "Release",
-                (OptLevel::Release, true) => "RelWithDebInfo",
-                (OptLevel::Size, _) => "MinSizeRel",
-            }
-        };
-
         println!(
             "cargo:rustc-link-search=native={}/build/src/libFLAC/{}",
             flac_path.display(),
-            profile
+            flac_config.get_profile()
         );
     } else {
         println!(
