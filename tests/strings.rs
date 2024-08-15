5f7958ca0b2b6e6f19b078cc20e904b7040d95f9
Matthias Geier <Matthias.Geier@gmail.com>
2024-08-15 14:29:06 +0200

    TEST: Don't use get_unchecked() to access zero-element array

--- tests/strings.rs
+++ tests/strings.rs
@@ -5,7 +5,11 @@ use libflac_sys as ffi;
 #[test]
 fn strings() {
     let idx = ffi::FLAC__STREAM_METADATA_PICTURE_TYPE_FISH as usize;
-    let ptr = unsafe { *ffi::FLAC__StreamMetadata_Picture_TypeString.get_unchecked(idx) };
+    let ptr = unsafe {
+        *ffi::FLAC__StreamMetadata_Picture_TypeString
+            .as_ptr()
+            .add(idx)
+    };
     let c_string = unsafe { CStr::from_ptr(ptr) };
     assert_eq!(c_string.to_str().unwrap(), "A bright coloured fish");
 }
